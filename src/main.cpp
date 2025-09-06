34792eb19ff6b027b61d2bd4e0870432a19b5fc7
Rafbill <158373519+Rafbillpc@users.noreply.github.com>
2025-09-06 14:28:33 +0200

    optimize the max clique

--- src/main.cpp
+++ src/main.cpp
@@ -4,182 +4,6 @@
 #include "backward.hpp"
 #include "kissat.h"
 
-struct max_clique {
-  int n;
-  vector<vector<unsigned char>> adj;
-
-  vector<vector<int>> es;
-  vector<vector<tuple<int,int> > > vs;
-
-  struct vertex {
-    vertex() = default;
-    vertex(int id_) {
-      id = id_;
-    }
-
-    int       id;
-    int       bound;
-  };
-
-  vector<vector<vertex>> P;
-  vector<int> level;
-
-  vector<int> colors;
-  vector<vector<vertex>> colorVertices;
-  vector<int> vertexColor;
-  vector<int> vertexExplored;
-
-  vector<int> maxClique;
-
-  max_clique(vector<vector<int>> G) {
-    maxClique = {0};
-
-    n = G.size();
-    adj.assign(n, vector<unsigned char>(n,0));
-    FOR(i,n) for(int j : G[i]) adj[i][j] = 1;
-
-    es.resize(1); vs.resize(1); vs[0].resize(n);
-
-    FOR(i,n) {
-      get<0>(vs[0][i]) = es[0].size();
-      FOR(j,n) {
-        if(adj[i][j]) {
-          es[0].emplace_back(j);
-        }
-      }
-      get<1>(vs[0][i]) = es[0].size();
-    }
-
-    P.resize(2);
-    level.assign(n,0);
-
-    colors.assign(n+3,0);
-    colorVertices.resize(n+3);
-    vertexColor.assign(n,0);
-    vertexExplored.assign(n,0);
-  }
-
-  void initial_sort(vector<int> const& s) {
-    int sz = s.size();
-    sort(all(P[sz]), [&](vertex const& a, vertex const& b) {
-      return get<1>(vs[sz][a.id]) - get<0>(vs[sz][a.id])
-        > get<1>(vs[sz][b.id]) - get<0>(vs[sz][b.id]);
-      });
-  }
-
-
-  void induce(vector<int> const& s) {
-    int sz = s.size();
-    if((int)es.size() == sz) {
-      es.emplace_back();
-      vs.emplace_back(n);
-    }
-    auto const& Pcur = P[sz];
-    auto const& esPrev = es[sz-1];
-    auto const& vsPrev = vs[sz-1];
-    auto& esCur = es[sz];
-    auto& vsCur = vs[sz];
-
-
-    esCur.clear();
-    FOR(w,Pcur.size()) {
-      int i = Pcur[w].id;
-      get<0>(vsCur[i]) = esCur.size();
-      int from = get<0>(vsPrev[i]), to = get<1>(vsPrev[i])-1;
-      FORU(ei,from,to) {
-        auto const& e = esPrev[ei];
-        if(level[e] == sz) {
-          esCur.pb(e);
-        }
-      }
-      get<1>(vsCur[i]) = esCur.size();
-    }
-  }
-
-  void color(vector<int> const& s) {
-    int sz = s.size();
-    int max_k = 1;
-    colors[1] = colors[2] = 0;
-    int date = 0;
-
-    FOR(w, P[sz].size()) {
-      int i = P[sz][w].id;
-      date += 1;
-      int from = get<0>(vs[sz][i]), to = get<1>(vs[sz][i])-1;
-
-      if(sz+1+(to-from+1) < (int)maxClique.size()+1) {
-        level[i]--;
-      }else{
-        FORU(jk,from,to) if(vertexExplored[es[sz][jk]]) colors[vertexColor[es[sz][jk]]] = date;
-
-        int k = 1; while(colors[k] == date) k += 1;
-        if(k > max_k) { max_k += 1; colors[max_k+1] = 0; }
-        vertexExplored[i] = 1; vertexColor[i] = k;
-        colorVertices[k].pb(P[sz][w]);
-      }
-    }
-
-    P[sz].clear();
-    FORU(k,1,max_k) {
-      for(auto const& v : colorVertices[k]) {
-        vertexExplored[v.id] = 0;
-        P[sz].pb(v);
-        P[sz].back().bound = k;
-      }
-      colorVertices[k].clear();
-    }
-  }
-
-  void searchAt(int i) {
-    vector<int> s; s.pb(i);
-    FOR(j,i) if(adj[i][j]) {
-      level[j]++;
-      P[1].pb(vertex { (int)j });
-    }
-    induce(s);
-    initial_sort(s);
-    color(s);
-    branch(s);
-    assert(P[1].empty());
-  }
-
-  void search(){
-    FOR(i,n) {
-      searchAt(i);
-    }
-  }
-
-  void branch(vector<int> const& s){
-    int sz = s.size();
-    while(!P[sz].empty()
-          && sz + P[sz].size() >= (maxClique.size()+1)) {
-      vertex v = P[sz].back();
-
-      level[v.id]--;
-      P[sz].pop_back();
-
-      if((int)P.size() == sz+1) P.emplace_back();
-
-      if(sz + v.bound <= (int)maxClique.size()) continue;
-      for(auto const& w : P[sz]) if(adj[v.id][w.id]) {
-          level[w.id]++;
-          P[sz+1].emplace_back(w);
-        }
-      vector<int> s2 = s; s2.pb(v.id);
-      if(sz+1 > (int)maxClique.size()) { maxClique = s2; }
-      induce(s2);
-      color(s2);
-      if(!P[sz+1].empty()) branch(s2);
-    }
-    while(!P[sz].empty()) {
-      vertex const& v = P[sz].back();
-      level[v.id]--;
-      P[sz].pop_back();
-    }
-  }
-};
-
-
 struct union_find {
   vector<int> A;
   vector<int> SZ;
@@ -293,23 +117,39 @@ layout solve(QUERIES const& Q, int size, int num_queries) {
     if(!imp) break;
   }
 
-  vector<int> maxClique;
-
-  FOR(T, 4) {
-    vector<int> E;
-    FOR(i, N) if(tag[i] == T) E.pb(i);
-
-    vector<vector<int>> G(E.size());
-    FOR(i, E.size()) FOR(j, i) if(DIFF[E[i]][E[j]]) {
-      G[i].pb(j);
-      G[j].pb(i);
+  map<vector<int>, vector<int>> cache;
+  auto max_clique = [&](auto &&max_clique, vector<int> elems) -> vector<int> {
+    if(elems.empty()) return {};
+    sort(all(elems));
+    if(cache.count(elems)) return cache[elems];
+    vector<int> part[4];
+    for(int i : elems) {
+      part[tag[i]].pb(i);
     }
-    if(!G.empty()) {
-      max_clique MC(G);
-      MC.search();
-      for(int i : MC.maxClique) maxClique.pb(E[i]);
+    vector<int> res;
+    FOR(t, 4) if(!part[t].empty()) {
+      vector<int> cur = {part[t][0]};
+      FOR(j, 6) {
+        vector<int> sub;
+        map<int,int> pre;
+        for(int i : part[t]) {
+          if(to[i][j] != -1) {
+            sub.pb(to[i][j]);
+            pre[to[i][j]] = i;
+          }
+        }
+        vector<int> tmp = max_clique(max_clique, sub);
+        if(tmp.size() > cur.size()) {
+          cur.clear();
+          for(int i : tmp) cur.pb(pre[i]);
+        }
+      }
+      res.insert(end(res), all(cur));
     }
-  }
+    return cache[elems] = res;
+  };
+  vector<int> E(N); iota(all(E),0);
+  auto maxClique = max_clique(max_clique, E);
 
   debug(maxClique.size());
   if((int)maxClique.size() < size) return {};
@@ -419,8 +259,8 @@ int main() {
 
   // test
   while(1) {
-    int size = 60;
-    int num_queries = 3;
+    int size = 18;
+    int num_queries = 1;
     layout L; L.generate(size);
     layout_queries Q(L);
     auto R = solve(Q, size, num_queries);
